f0e5789d9b0ca17af51e54a074685e9b6f80571a
POLICITTY SHALINI <142616743+POLICITTYSHALINI@users.noreply.github.com>
2023-08-24 16:14:17 +0530

    Add files via upload

---
+++ todolist.py
@@ -0,0 +1,67 @@
+# TO DO LIST
+import tkinter
+import tkinter.messagebox
+import pickle
+
+root = tkinter.Tk()
+root.title("TO-DO-LIST ")
+
+def add_task():
+    task = entry_task.get()
+    if task !="":
+        listbox_tasks.insert(tkinter.END, task)
+        entry_task.delete(0, tkinter.END)
+    else:
+        tkinter.messagebox.showwarning(title="WARNING", message=" SELECT A TASK")
+
+def delete_task():
+    try:
+        task_index = listbox_tasks.curselection()[0]
+        listbox_tasks.delete(task_index)
+    except:
+        tkinter.messagebox.showwarning(title="WARNING", message="SELECT A TASK")
+
+def load_task():
+    try:
+        tasks = pickle.load(open("tasks.dat", "rb"))
+        listbox_tasks.delete(0, tkinter.END)
+        for task in tasks:
+            listbox_tasks.insert(tkinter.END, task)
+    except:
+        tkinter.messagebox.showwarning(title="WARNING", message="CANNOT FIND TASKS.dat.")
+
+def save_tasks():
+    tasks = listbox_tasks.get(0, listbox_tasks.size())
+    pickle.dump(tasks, open("tasks.dat", "wb"))
+
+
+frame_tasks = tkinter.Frame(root)
+frame_tasks.pack()
+
+listbox_tasks = tkinter.Listbox(frame_tasks, height=5, width=40)
+listbox_tasks.pack(side=tkinter.LEFT)
+
+scrollbar_tasks = tkinter.Scrollbar(frame_tasks)
+scrollbar_tasks.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+
+listbox_tasks.config(yscrollcommand=scrollbar_tasks.set)
+scrollbar_tasks.config(command=listbox_tasks.yview)
+
+entry_task = tkinter.Entry(root, width=45)
+entry_task.pack()
+
+button_add_task =tkinter.Button(root, text="ADD TASK", width=45, command=add_task)
+button_add_task.pack()
+
+button_delete_task =tkinter.Button(root, text="DELETE TASK", width=45, command=delete_task)
+button_delete_task.pack()
+
+button_load_tasks =tkinter.Button(root, text="LOAD TASKS", width=45, command=load_task)
+button_load_tasks.pack()
+
+button_save_tasks =tkinter.Button(root, text="SAVE TASKS", width=45, command=save_tasks)
+button_save_tasks.pack()
+
+
+
+root.mainloop()
